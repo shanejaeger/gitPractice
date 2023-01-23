b6a86bd17ddaad226b97746273570b34a68c6a64
Shane <jaegersh@bc.edu>
2023-01-23 13:35:15 -0500

    Initial Commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Erik JAEGER on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

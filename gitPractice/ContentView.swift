a4b9c5d7c5cc6385f0445009800070eda8a75264
Shane <jaegersh@bc.edu>
2023-01-23 13:45:26 -0500

    v1.0

--- gitPractice/ContentView.swift
+++ gitPractice/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("V1.0")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
         }
         .padding()
+        .foregroundColor(.red)
     }
 }
 
